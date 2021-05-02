4c7c5b9933d55b713cf5f75944930df16ce34314
Ame-ReIori <ame_reiori@outlook.com>
2021-05-02 23:57:52 +0800

    add str compare function

--- test/test.h
+++ test/test.h
@@ -6,4 +6,11 @@
 #include <emp-tool/utils/prg.h> // prg
 #include <emp-tool/utils/utils.h> // time
 
+bool cmp_str(const uint8_t *a, const uint8_t *b, int len) {
+    for (int i = 0; i < len; i++) {
+        if (a[i] != b[i]) return false;
+    }
+    return true;
+}
+
 #endif
