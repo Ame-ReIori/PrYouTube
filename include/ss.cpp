960780936b608c22e72053b2ef4a1ebac0106c60
Ame-ReIori <reiori_ame@outlook.com>
2021-05-09 16:52:33 +0800

    add secret share scheme

---
+++ include/ss.cpp
@@ -0,0 +1,10 @@
+#include "./ss.h"
+
+void Shr(Matrix32u &s0, Matrix32u &s1, Matrix32u &s) {
+    random_matrix32u(s0);
+    s1 = s - s0;
+}
+
+void Rec(Matrix32u &s, Matrix32u &s0, Matrix32u &s1) {
+    s = s0 + s1;
+}
---
+++ include/ss.h
@@ -0,0 +1,11 @@
+#ifndef __SS_H
+#define __SS_H
+
+#include "./common.h"
+#include "./utils.h"
+
+void Shr(Matrix32u &s0, Matrix32u &s1, Matrix32u &s);
+
+void Rec(Matrix32u &s, Matrix32u &s0, Matrix32u &s1);
+
+#endif
